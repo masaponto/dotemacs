34c18f34c8b0ef3f004c8b278d55ba659f760df6
masaponto <masaponto@gmail.com>
2019-08-25 21:05:57 +0900

    [Add] package-archives repo

--- init.el
+++ init.el
@@ -4,10 +4,12 @@
 
 ;; package
 (require' package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") )
-(package-initialize)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
+(package-initialize)
 
 ;; init-loader
 (require 'init-loader)
@@ -24,3 +26,21 @@
 ; use-package
 (unless (require 'use-package nil t)
   (defmacro use-package (&rest args)))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-themes-enable-bold t)
+ '(doom-themes-enable-italic t)
+ '(package-selected-packages
+   (quote
+    (company-quickhelp dumb-jump doom-themes company-jedi company auto-virtualenvwrapper web-mode quelpa-use-package markdown-mode init-loader indent-guide helm-ag haskell-mode elscreen anzu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-bar ((t (:background "#6272a4")))))
