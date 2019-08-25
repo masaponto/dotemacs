8846a41d67004fe0abbc6a1eab43553f22ee7825
masaponto <masaponto@gmail.com>
2019-08-25 23:48:45 +0900

    [Fix]

--- config/30_markdown.el
+++ config/30_markdown.el
@@ -3,11 +3,13 @@
   :defer t
   :mode "\\.md\\'"
   :config
+  ;; do not work well
   (defun cleanup-org-tables ()
     (save-excursion
       (goto-char (point-min))
       (while (search-forward "-+-" nil t) (replace-match "-|-"))))
-  (add-hook 'markdown-mode-hook 'orgtbl-mode)
+  (add-hook 'markdown-mode-hook 'turn-on-orgtbl)
+  ;;(add-hook 'markdown-mode-hook 'orgtbl-mode)
   (add-hook 'markdown-mode-hook
             #'(lambda()
                 (add-hook 'after-save-hook 'cleanup-org-tables  nil 'make-it-local))))
