4259139fec776175023199f0a161e0e034acf4fd
masaponto <masaponto@gmail.com>
2019-08-25 21:06:16 +0900

    [Add] use

---
+++ config/10_dumb-jump.el
@@ -0,0 +1,9 @@
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config (setq dumb-jump-selector 'helm
+                dumb-jump-force-searcher 'ag) ;; (setq dumb-jump-selector 'helm)
+  :ensure)
